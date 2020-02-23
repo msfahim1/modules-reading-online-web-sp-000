2bb866bba54c513fb51b689ff95400c80aac4a73
Michael Fahim <michael_fahim@yahoo.com>
2020-02-23 00:05:25 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -3,4 +3,10 @@ module MetaDancing
   def metadata
     "This class produces objects that love to dance."
   end
+  class Kid
+  extend MetaDancing
+end
+class Dancer
+  extend MetaDancing
+end
 end
