4ca32c4f82e24c1a8949d1541d16e4df338deb43
Michael Fahim <michael_fahim@yahoo.com>
2020-02-23 00:12:31 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,10 +1,14 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
-class Dancer
-  include Dance
+require_relative './fancy_dance.rb'
 
-  extend MetaDancing
+
+class Dancer
+
+
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
 
   attr_accessor :name
 
